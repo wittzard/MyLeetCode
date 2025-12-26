6bb1bfa21bd2b6148fcbfb25e520fea27c555812
Witawat Kanhasiri <68972955+wittzard@users.noreply.github.com>
2025-12-26 15:10:55 +0700

    Time: 160 ms (5.22%), Space: 26.7 MB (90.12%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,7 +1,7 @@
 import math
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfitDP(self, prices: List[int]) -> int:
         n = len(prices)
         if n == 1:
             return 0
@@ -15,6 +15,21 @@ class Solution:
         for i in range(1,n):
             dp[i] = min(dp[i-1], prices[i])
             profit = max(profit, prices[i]-dp[i])
+
+        return profit
+
+    # reduce space
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        if n == 1:
+            return 0
+
+        profit = -math.inf
+        min_sofar = prices[0]
+
+        for i in range(1,n):
+            min_sofar = min(min_sofar, prices[i])
+            profit = max(profit, prices[i] - min_sofar)
 
         return profit
 
