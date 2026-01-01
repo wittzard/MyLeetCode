cdd179a1be9cd0099ae6075df430a9e0757144db
Witawat Kanhasiri <68972955+wittzard@users.noreply.github.com>
2026-01-01 23:46:20 +0700

    Time: 0 ms (100%), Space: 17.5 MB (87.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,19 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        valid = ['()','{}','[]']
+        mapping = {
+            ')':'(',
+            '}':'{',
+            ']':'['
+        }
 
         for char in s:
-            if char in ['(','{','[']:
-                stack.append(char)
-            else:
-                if not stack: # check if close bracket and stack is empty
+            if char in mapping:
+                # if it close bracket check if stack is empty, check its couple
+                if not stack or stack.pop() != mapping[char]:
                     return False
-                else:
-                    if stack.pop() + char not in valid:
-                        return False
-        
-        return not stack # check if stack is empty if it have remaining like '((('
-                    
+            else:
+                stack.append(char)
+
+        return not stack # check case open bracket only '(((('                  
 
         
